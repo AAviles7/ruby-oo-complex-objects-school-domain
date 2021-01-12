17b51931dd122bd95d565cf1b166e6b7b08ad9fd
Abraham Aviles <aaaviles1995@gmail.com>
2021-01-12 17:30:22 -0500

    commit tree 8280530fa42f694e14f9be1af4746bcb937728e3

--- lib/school.rb
+++ lib/school.rb
@@ -12,10 +12,8 @@ class School
     def add_student(name, grade)
         if !(@roster[grade])
             @roster[grade] = []
-            @roster[grade] << (name)
-        else
-            @roster[grade] << (name)
         end
+        @roster[grade] << (name)
     end
 
     def grade(grade)
